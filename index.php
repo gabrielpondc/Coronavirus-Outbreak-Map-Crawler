68d6ca62cba2f1fdb1781a7b4d525b02e18a6e97
Gabriel.C.Pond <gabrielpondc@yahoo.com>
2020-02-01 00:33:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <?php
 DEFINE ('DB_USER','root');
+DEFINE ('DB_PASSWORD','gjk19961226');
 DEFINE ('DB_HOST','localhost');
 DEFINE ('DB_NAME','virus');
 $dbc=@mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('Could not to connect to Mysql:'.mysqli_connect_error());
@@ -14,7 +15,7 @@ $resulta=mysqli_fetch_array($resa);
 $sqlc="select dia,sus,de,res  from  dom";
 $resc=mysqli_query($dbc,$sqlc);
 $resultc=mysqli_fetch_array($resc);
-$sqld="select location,dia  from  global";
+$sqld="select location,dia,res,de from  global";
 $resd=mysqli_query($dbc,$sqld);
 $resultd=mysqli_fetch_array($resd);
 $sqle="select id,con  from  news";
@@ -35,6 +36,7 @@ $resultg=mysqli_fetch_array($resg);
 <script src="js/jquery-1.11.1.min.js"></script>
 <script src="js/echarts.min.js"></script>
 <script src="js/china.js"></script>
+<script type="text/javascript" src="js/world.js"></script>
 <link rel="stylesheet" href="//cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="//cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
 <script src="//cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -6254,23 +6256,7 @@ fAuoAf8/e7OFcXw73qMAAAAASUVORK5CYII='style="max-width:100%;"/>
                 ?>
 
             </table>
-            <h4>境外</h4>
-            <table  width='100%' style="font-size: 15px;"rules=none frame=void class="table-bordered">
-                <tr>
-                <?php
-                foreach ($resd as $row){
-                    echo "<th style=\"text-align: center;\"><span style=\"color: #4d5054\">{$row['location']}</span></th>";
-                };
-                ?>
-                </tr>
-                <tr>
-                <?php
-                foreach ($resd as $row){
-                    echo "<td align=\"center\"><span style=\"color: #fb776b\">{$row['dia']}</span></td>";
-                };
-                ?>
-                </tr>
-            </table>
+
             <div id="mainMap" style="height:633px;width:auto;padding:10px;background:white;margin:20px 0 0;position: relative;top: 25%;"></div>
 
             <table  width='100%' style="font-size: 25px;" class="table-bordered">
@@ -6327,6 +6313,7 @@ fAuoAf8/e7OFcXw73qMAAAAASUVORK5CYII='style="max-width:100%;"/>
                 echo "</tr>";
             };
             ?>
+        </table>
             <div id="js" style="width:auto;height:660px;margin:20px;position:relative;top: 25%;"></div>
             <table  width='100%' style="font-size: 25px;" class="table-bordered">
 
@@ -6477,6 +6464,316 @@ fAuoAf8/e7OFcXw73qMAAAAASUVORK5CYII='style="max-width:100%;"/>
     myChart.setOption(optionMap);
 
 </script>
+<div style="text-align: center">
+<div class="panel panel-danger">
+    <div class="panel-heading">
 
+        <h1 class="panel-title">世界疫情情况</h1>
+    </div>
+    <div class="panel-body">
+        <h2>境外</h2>
+        <div id="global" style="width: auto;height: 450px;margin:20px;position:relative;top: 25%;"></div>
+        <table  width='100%' style="font-size: 25px;" class="table-bordered">
+
+            <tr>
+                <th style="text-align: center;background: #f5f6f7"><span style="color:rgba(0, 0, 0, 0.65)"> 地区</span></th>
+                <th style="text-align: center;background:  #ff3131"><span style="color: #fff">确诊</span></th>
+                <th style="text-align: center;background:  #178b50"><span style="color: #fff">治愈</span></th>
+                <th style="text-align: center;background:  #4d5054"><span style="color:#fff">死亡</span></th>
+            </tr>
+
+            <?php
+            foreach ($resd as $row){
+                echo "<tr>";
+                echo "<td style='background: #10aeb5;font-style: inherit; font-family: inherit;' align=\"center\"><span style=\"color:#fff\">{$row['location']}</span></td>";
+                echo "<td align=\"center\"><span style=\"color: #ff3131\">{$row['dia']}</span>&nbsp&nbsp&nbsp例</td>";
+                echo "<td align=\"center\"><span style=\"color: #178b50\">{$row['res']}</span>&nbsp&nbsp&nbsp例</td>";
+                echo "<td align=\"center\"><span style=\"color: #4d5054\">{$row['de']}</span>&nbsp&nbsp&nbsp例</td>";
+
+                echo "</tr>";
+            };
+            ?>
+            </h2>
+
+        </table>
+
+        <script>
+
+            //初始化echarts实例
+            var myChart = echarts.init(document.getElementById('global'));
+            //使用制定的配置项和数据显示图表
+            myChart.setOption({
+                // backgroundColor: "#02AFDB",
+                title: {
+                    // text: '在线设备分布',
+                    left: '40%',
+                    top: '0px',
+                    textStyle: {
+                        color: '#fff',
+                        opacity: 0.7
+                    }
+                },
+                nameMap:{
+                    "Afghanistan": "阿富汗",
+                    "Albania": "阿尔巴尼亚",
+                    "Algeria": "阿尔及利亚",
+                    "Angola": "安哥拉",
+                    "Argentina": "阿根廷",
+                    "Armenia": "亚美尼亚",
+                    "Australia": "澳大利亚",
+                    "Austria": "奥地利",
+                    "Azerbaijan": "阿塞拜疆",
+                    "Bahamas": "巴哈马",
+                    "Bahrain": "巴林",
+                    "Bangladesh": "孟加拉国",
+                    "Belarus": "白俄罗斯",
+                    "Belgium": "比利时",
+                    "Belize": "伯利兹",
+                    "Benin": "贝宁",
+                    "Bhutan": "不丹",
+                    "Bolivia": "玻利维亚",
+                    "Bosnia and Herz.": "波斯尼亚和黑塞哥维那",
+                    "Botswana": "博茨瓦纳",
+                    "Brazil": "巴西",
+                    "British Virgin Islands": "英属维京群岛",
+                    "Brunei": "文莱",
+                    "Bulgaria": "保加利亚",
+                    "Burkina Faso": "布基纳法索",
+                    "Burundi": "布隆迪",
+                    "Cambodia": "柬埔寨",
+                    "Cameroon": "喀麦隆",
+                    "Canada": "加拿大",
+                    "Cape Verde": "佛得角",
+                    "Cayman Islands": "开曼群岛",
+                    "Central African Rep.": "中非共和国",
+                    "Chad": "乍得",
+                    "Chile": "智利",
+                    "China": "中国",
+                    "Colombia": "哥伦比亚",
+                    "Comoros": "科摩罗",
+                    "Congo": "刚果",
+                    "Costa Rica": "哥斯达黎加",
+                    "Croatia": "克罗地亚",
+                    "Cuba": "古巴",
+                    "Cyprus": "塞浦路斯",
+                    "Czech Rep.": "捷克共和国",
+                    "Côte d'Ivoire": "科特迪瓦",
+                    "Dem. Rep. Congo": "刚果民主共和国",
+                    "Dem. Rep. Korea": "朝鲜",
+                    "Denmark": "丹麦",
+                    "Djibouti": "吉布提",
+                    "Dominican Rep.": "多米尼加共和国",
+                    "Ecuador": "厄瓜多尔",
+                    "Egypt": "埃及",
+                    "El Salvador": "萨尔瓦多",
+                    "Equatorial Guinea": "赤道几内亚",
+                    "Eritrea": "厄立特里亚",
+                    "Estonia": "爱沙尼亚",
+                    "Ethiopia": "埃塞俄比亚",
+                    "Falkland Is.": "福克兰群岛",
+                    "Fiji": "斐济",
+                    "Finland": "芬兰",
+                    "Fr. S. Antarctic Lands": "所罗门群岛",
+                    "France": "法国",
+                    "Gabon": "加蓬",
+                    "Gambia": "冈比亚",
+                    "Georgia": "格鲁吉亚",
+                    "Germany": "德国",
+                    "Ghana": "加纳",
+                    "Greece": "希腊",
+                    "Greenland": "格陵兰",
+                    "Guatemala": "危地马拉",
+                    "Guinea": "几内亚",
+                    "Guinea-Bissau": "几内亚比绍",
+                    "Guyana": "圭亚那",
+                    "Haiti": "海地",
+                    "Honduras": "洪都拉斯",
+                    "Hungary": "匈牙利",
+                    "Iceland": "冰岛",
+                    "India": "印度",
+                    "Indonesia": "印度尼西亚",
+                    "Iran": "伊朗",
+                    "Iraq": "伊拉克",
+                    "Ireland": "爱尔兰",
+                    "Isle of Man": "马恩岛",
+                    "Israel": "以色列",
+                    "Italy": "意大利",
+                    "Jamaica": "牙买加",
+                    "Japan": "日本",
+                    "Jordan": "约旦",
+                    "Kazakhstan": "哈萨克斯坦",
+                    "Kenya": "肯尼亚",
+                    "Korea": "韩国",
+                    "Kuwait": "科威特",
+                    "Kyrgyzstan": "吉尔吉斯斯坦",
+                    "Lao PDR": "老挝",
+                    "Latvia": "拉脱维亚",
+                    "Lebanon": "黎巴嫩",
+                    "Lesotho": "莱索托",
+                    "Liberia": "利比里亚",
+                    "Libya": "利比亚",
+                    "Lithuania": "立陶宛",
+                    "Luxembourg": "卢森堡",
+                    "Macedonia": "马其顿",
+                    "Madagascar": "马达加斯加",
+                    "Malawi": "马拉维",
+                    "Malaysia": "马来西亚",
+                    "Maldives": "马尔代夫",
+                    "Mali": "马里",
+                    "Malta": "马耳他",
+                    "Mauritania": "毛利塔尼亚",
+                    "Mauritius": "毛里求斯",
+                    "Mexico": "墨西哥",
+                    "Moldova": "摩尔多瓦",
+                    "Monaco": "摩纳哥",
+                    "Mongolia": "蒙古",
+                    "Montenegro": "黑山共和国",
+                    "Morocco": "摩洛哥",
+                    "Mozambique": "莫桑比克",
+                    "Myanmar": "缅甸",
+                    "Namibia": "纳米比亚",
+                    "Nepal": "尼泊尔",
+                    "Netherlands": "荷兰",
+                    "New Caledonia": "新喀里多尼亚",
+                    "New Zealand": "新西兰",
+                    "Nicaragua": "尼加拉瓜",
+                    "Niger": "尼日尔",
+                    "Nigeria": "尼日利亚",
+                    "Norway": "挪威",
+                    "Oman": "阿曼",
+                    "Pakistan": "巴基斯坦",
+                    "Panama": "巴拿马",
+                    "Papua New Guinea": "巴布亚新几内亚",
+                    "Paraguay": "巴拉圭",
+                    "Peru": "秘鲁",
+                    "Philippines": "菲律宾",
+                    "Poland": "波兰",
+                    "Portugal": "葡萄牙",
+                    "Puerto Rico": "波多黎各",
+                    "Qatar": "卡塔尔",
+                    "Reunion": "留尼旺",
+                    "Romania": "罗马尼亚",
+                    "Russia": "俄罗斯",
+                    "Rwanda": "卢旺达",
+                    "S. Geo. and S. Sandw. Is.": "南乔治亚和南桑威奇群岛",
+                    "S. Sudan": "南苏丹",
+                    "San Marino": "圣马力诺",
+                    "Saudi Arabia": "沙特阿拉伯",
+                    "Senegal": "塞内加尔",
+                    "Serbia": "塞尔维亚",
+                    "Sierra Leone": "塞拉利昂",
+                    "Singapore": "新加坡",
+                    "Slovakia": "斯洛伐克",
+                    "Slovenia": "斯洛文尼亚",
+                    "Solomon Is.": "所罗门群岛",
+                    "Somalia": "索马里",
+                    "South Africa": "南非",
+                    "Spain": "西班牙",
+                    "Sri Lanka": "斯里兰卡",
+                    "Sudan": "苏丹",
+                    "Suriname": "苏里南",
+                    "Swaziland": "斯威士兰",
+                    "Sweden": "瑞典",
+                    "Switzerland": "瑞士",
+                    "Syria": "叙利亚",
+                    "Tajikistan": "塔吉克斯坦",
+                    "Tanzania": "坦桑尼亚",
+                    "Thailand": "泰国",
+                    "Togo": "多哥",
+                    "Tonga": "汤加",
+                    "Trinidad and Tobago": "特立尼达和多巴哥",
+                    "Tunisia": "突尼斯",
+                    "Turkey": "土耳其",
+                    "Turkmenistan": "土库曼斯坦",
+                    "U.S. Virgin Islands": "美属维尔京群岛",
+                    "Uganda": "乌干达",
+                    "Ukraine": "乌克兰",
+                    "United Arab Emirates": "阿拉伯联合酋长国",
+                    "United Kingdom": "英国",
+                    "United States": "美国",
+                    "Uruguay": "乌拉圭",
+                    "Uzbekistan": "乌兹别克斯坦",
+                    "Vanuatu": "瓦努阿图",
+                    "Vatican City": "梵蒂冈城",
+                    "Venezuela": "委内瑞拉",
+                    "Vietnam": "越南",
+                    "W. Sahara": "西撒哈拉",
+                    "Yemen": "也门",
+                    "Yugoslavia": "南斯拉夫",
+                    "Zaire": "扎伊尔",
+                    "Zambia": "赞比亚",
+                    "Zimbabwe": "津巴布韦"
+                },
+                dataRange: {
+                    show: true,
+                    min: 0,
+                    max: 15,
+                    text: ['高', '低'],
+                    realtime: true,
+                    calculable: true,
+                    color: ['#5d1607','#a21508', '#e75151', '#e7b1af']
+                },
+                tooltip: {
+
+                    trigger: 'item'
+                },
+                geo: {
+                    map: 'world',
+                    label: {
+                        title:{text:'确诊'},
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    roam: false,
+                    silent: true,
+                    itemStyle: {
+                        normal: {
+                            areaColor: '#37376e',
+                            borderColor: '#000'
+                        },
+                        emphasis: {
+                            areaColor: '#2a333d'
+                        }
+                    }
+                },
+                series: [{
+                    type: 'map',
+                    mapType: 'world',
+                    // zoom: 1.2,
+                    mapLocation: {
+                        y: 100
+                    },
+                    data: [
+                        <?php
+                        foreach ($resd as $row){
+                            echo "{name: '{$row['location']}',value: {$row['dia']}},";
+                        };
+                        ?>
+                        {name:'中国',value:<?php echo $resultc['dia'];?>}
+
+                    ],
+                    symbolSize: 12,
+                    label: {
+                        title:{text:'确诊'},
+                        normal: {
+                            show: false
+                        },
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    itemStyle: {
+                        emphasis: {
+                            borderColor: '#fff',
+                            borderWidth: 1
+                        }
+                    }
+                }],
+            });
+        </script>
+</div>
+</div>
+</div>
 </body>
 </html>
