5740bd49c164d36b4fd050b3c092aa89121683d4
Gabriel.C.Pond <gabrielpondc@yahoo.com>
2020-06-13 01:37:03 +0900

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,10 +3,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <iostream>
+#include <errno.h>
 
-#define CUDA 0
-#define OPENMP 1
 #define SPHERES 20
 
 #define rnd( x ) (x * rand() / RAND_MAX)
@@ -17,11 +15,11 @@ struct Sphere {
 	float   r, b, g;
 	float   radius;
 	float   x, y, z;
-	float hit(float ox, float oy, float* n) {
+	float hit(float ox, float oy, float *n) {
 		float dx = ox - x;
 		float dy = oy - y;
-		if (dx * dx + dy * dy < radius * radius) {
-			float dz = sqrtf(radius * radius - dx * dx - dy * dy);
+		if (dx*dx + dy * dy < radius*radius) {
+			float dz = sqrtf(radius*radius - dx * dx - dy * dy);
 			*n = dz / sqrtf(radius * radius);
 			return dz + z;
 		}
@@ -39,7 +37,7 @@ void kernel(int x, int y, Sphere* s, unsigned char* ptr)
 
 	float r = 0, g = 0, b = 0;
 	float   maxz = -INF;
-	for (int i = 0; i < SPHERES; i++) {
+	for (int i = 0; i<SPHERES; i++) {
 		float   n;
 		float   t = s[i].hit(ox, oy, &n);
 		if (t > maxz) {
@@ -63,8 +61,8 @@ void ppm_write(unsigned char* bitmap, int xdim, int ydim, FILE* fp)
 	fprintf(fp, "P3\n");
 	fprintf(fp, "%d %d\n", xdim, ydim);
 	fprintf(fp, "255\n");
-	for (y = 0; y < ydim; y++) {
-		for (x = 0; x < xdim; x++) {
+	for (y = 0; y<ydim; y++) {
+		for (x = 0; x<xdim; x++) {
 			i = x + y * xdim;
 			fprintf(fp, "%d %d %d ", bitmap[4 * i], bitmap[4 * i + 1], bitmap[4 * i + 2]);
 		}
@@ -75,25 +73,21 @@ void ppm_write(unsigned char* bitmap, int xdim, int ydim, FILE* fp)
 int main(int argc, char* argv[])
 {
 	int option;
-	int x, y;
+
 	unsigned char* bitmap;
 
 	srand(time(NULL));
-	FILE* fp = fopen(argv[2], "w");
-	errno_t cw = fopen_s(&fp, argv[1], "w");
-	if (cw != 0) {
+
+	FILE *fp;
+
+	errno_t err = fopen_s(&fp, argv[1], "w");
+
+	if (err != 0) {
 		exit(-1);
 	}
 
-
-	if (strcmp(argv[1], "0") == 0) option = CUDA;
-	else {
-		option = OPENMP;
-		no_threads = atoi(argv[1]);
-	}
-
-	Sphere* temp_s = (Sphere*)malloc(sizeof(Sphere) * SPHERES);
-	for (int i = 0; i < SPHERES; i++) {
+	Sphere *temp_s = (Sphere*)malloc(sizeof(Sphere) * SPHERES);
+	for (int i = 0; i<SPHERES; i++) {
 		temp_s[i].r = rnd(1.0f);
 		temp_s[i].g = rnd(1.0f);
 		temp_s[i].b = rnd(1.0f);
@@ -102,18 +96,29 @@ int main(int argc, char* argv[])
 		temp_s[i].z = rnd(2000.0f) - 1000;
 		temp_s[i].radius = rnd(200.0f) + 40;
 	}
-	clock_t kssj = clock();
-	bitmap = (unsigned char*)malloc(sizeof(unsigned char) * DIM * DIM * 4);
-	
-	for (x = 0; x < DIM; x++)
-		for (y = 0; y < DIM; y++) kernel(x, y, temp_s, bitmap);
-	clock_t jssj = clock();
+
+	bitmap = (unsigned char*)malloc(sizeof(unsigned char)*DIM*DIM * 4);
+
+	clock_t start_clock = clock();
+
+	for (int x = 0; x < DIM; x++) {
+		for (int y = 0; y < DIM; y++) {
+			kernel(x, y, temp_s, bitmap);
+		}
+	}
+
+	clock_t end_clock = clock();
+
+
 	ppm_write(bitmap, DIM, DIM, fp);
 
 	fclose(fp);
 	free(bitmap);
 	free(temp_s);
-	double yxsj = ((double)(jssj - kssj)) / CLOCKS_PER_SEC;
-	std::cout << "OpenMP (" << 1 << " threads) ray tracing:" << yxsj << " sec" << std::endl;
+
+	double execute_time = ((double)(end_clock - start_clock)) / CLOCKS_PER_SEC;
+
+	printf_s("Single thread ray tracing: %f sec", execute_time);
+
 	return 0;
 }
